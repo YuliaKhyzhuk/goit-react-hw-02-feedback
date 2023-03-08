962d1e993d946f8c96f8fdadeba9f02a8301aaf0
YuliaKhyzhuk <anastezy1@gmail.com>
2023-03-08 03:48:43 +0200

    Basic structure created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+
+  render() {
+    return (
+      <div className="FeedbackContainer">
+        <h2 className="FeedbackContainerTitle">Please leave feedback</h2>
+        <div className="FeedbackButtons">
+          <button type="button" className="FeedbackButtonGood" onClick={() => {}}>Good</button>
+          <button type="button" className="FeedbackButtonNeutral" onClick={this.props.decrement}>Neutral</button>
+          <button type="button" className="FeedbackButtonBad" onClick={this.props.decrement}>Bad</button>
+        </div>
+        <h3 className="StatisticsTitle">Statistics</h3>
+        <p className="StatisticsData">Good:</p>
+        <p className="StatisticsData">Neutral:</p>
+        <p className="StatisticsData">Bad:</p>
+        <p className="StatisticsData">Total:</p>
+        <p className="StatisticsData">Positive feedback: %</p>
+
+      </div>
+    );
+  }
+}
+
+// export default App;
